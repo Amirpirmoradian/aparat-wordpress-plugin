84821a495858bf15ba9b7845c02509250045ab0b
Amir Pirmoradian <amirpirmoradian94@gmail.com>
2021-07-19 13:08:00 +0430

    init

---
+++ piramir-aparat-widget.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Piramir Aparat
+ */
+/*
+Plugin Name: Piramir - Aparat widget
+Plugin URI: https://piramir.ir/aparat-widget
+Description: Add aparat widget using shortcodes. showing your latest videos on aparat in your homepage
+Version: 1.0.0
+Author: Piramir
+Author URI: https://piramir.ir
+License: GPLv2 or later
+Text Domain: piramir
+*/
